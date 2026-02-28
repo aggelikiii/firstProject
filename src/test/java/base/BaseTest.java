3a4c3f71ef53d6ba060b925d32a45bcdc89fd350
Aggeliki Galanopoulou <aggeliki6298@gmail.com>
2026-02-28 23:56:58 +0100

    Added GooglePage and new test case

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -8,6 +8,12 @@ import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
 
     protected WebDriver driver;
@@ -16,15 +22,11 @@ public class BaseTest {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        driver.quit();
     }
 }
